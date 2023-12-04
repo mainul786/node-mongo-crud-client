0bbf6a06a0fd89e5d7aeb17dc7d3313bdc7b3cab
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-12-04 21:51:55 +0530

    put method applyed successfully

--- src/component/Update.js
+++ src/component/Update.js
@@ -5,10 +5,24 @@ const Update = () => {
     const storedUser = useLoaderData();
     console.log(storedUser);
 
-    const [users, setUsers] = useState({})
+    const [users, setUsers] = useState(storedUser)
     const handleUpdateUser = event => {
         event.preventDefault();
-        console.log(users)
+        console.log(users);
+        fetch(`http://localhost:5000/users/${storedUser._id}`,{
+            method:'PUT',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(users)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data)
+            if(data.modifiedCount > 0){
+                alert('updated successfully')
+            }
+        })
     }
 
     const handleInputChange = event => {
@@ -24,7 +38,7 @@ const Update = () => {
 
             <form onSubmit={handleUpdateUser}>
                 <input type="text" onChange={handleInputChange} defaultValue={storedUser.name} name='name' placeholder='name' required /><br />
-                <input type="text" onChange={handleInputChange} defaultValue={storedUser.address} name='adress' placeholder='address' required /><br />
+                <input type="text" onChange={handleInputChange} defaultValue={storedUser.address} name='address' placeholder='address' required /><br />
                 <input type="email" onChange={handleInputChange} defaultValue={storedUser.email} name='email' placeholder='email' required /><br />
                 <button type="submit">Update User</button>
             </form>
